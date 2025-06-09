4ce5a8dc94b9079c116fa44ebaa86aa2385a5e30
atatendo <atatendo@vbox.dom>
2025-06-10 00:30:39 +0700

    prod prepare work

--- backend/app/Http/Controllers/Api/EventController.php
+++ backend/app/Http/Controllers/Api/EventController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Event;
-use Illuminate\Routing\Controller;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
